6c38c7711ca51e0bb4cd5aa4964782926b436dfb
Fatime Hashim <fatimemachira@gmail.com>
2024-01-26 18:42:23 +0300

    splash screen enabled

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main()
 {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
@@ -13,4 +17,5 @@ void main()
     ),
   ),
   );
+  FlutterNativeSplash.remove();
 }
